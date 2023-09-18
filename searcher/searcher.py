edef473dbe773a85be7e8a178d2e3e9d58c77202
timerke <timerke@mail.ru>
2023-09-18 17:16:51 +0300

    Added real time print of scanned files number

--- searcher/searcher.py
+++ searcher/searcher.py
@@ -20,10 +20,12 @@ class Searcher:
                 os.remove(obj_path)
             ut.print_(f"'{obj_path}' removed")
 
-    def _search(self, dir_path: str, files_and_dirs: List[str]) -> None:
+    def _search(self, dir_path: str, files_and_dirs: List[str], total_number: int) -> int:
         """
         :param dir_path: directory to search in;
-        :param files_and_dirs: list in which to place found paths of files and directories.
+        :param files_and_dirs: list in which to place found paths of files and directories;
+        :param total_number:
+        :return:
         """
 
         for obj_name in os.listdir(dir_path):
@@ -32,12 +34,15 @@ class Searcher:
             if is_junk:
                 self._junk.append(obj_path)
                 ut.print_(f"Junk found: '{obj_path}'")
+            total_number += 1
+            ut.print_(f"Number of scanned files and folders: {total_number}", same_place=True)
             files_and_dirs.append(obj_path)
             if os.path.isdir(obj_path) and not is_junk:
                 try:
-                    self._search(obj_path, files_and_dirs)
+                    total_number = self._search(obj_path, files_and_dirs, total_number)
                 except PermissionError:
                     ut.print_(f"Access denied to '{obj_path}'")
+        return total_number
 
     def print_junk(self) -> None:
         ut.print_("\nFound junk files and directories:")
@@ -62,4 +67,4 @@ class Searcher:
         self._junk.clear()
         if dir_path is None:
             dir_path = os.curdir
-        self._search(dir_path, files_and_dirs)
+        self._search(dir_path, files_and_dirs, 0)
--- searcher/utils.py
+++ searcher/utils.py
@@ -1,2 +1,15 @@
+last_line_length = 0
+same_line = False
+
+
 def print_(string: str, **kwargs) -> None:
-    print(string, **kwargs, flush=True)
+    global last_line_length, same_line
+
+    if "same_place" in kwargs:
+        print(f"{string}\r", flush=True, end="")
+        last_line_length = len(string)
+        same_line = True
+    else:
+        if same_line:
+            print(" " * last_line_length, flush=True, end="\r")
+        print(string, **kwargs, flush=True)
