b99dd843706d8e25e4dd1217c9811c769427f495
timerke <timerke@mail.ru>
2023-09-29 13:58:23 +0300

    Added test

--- tests/test_configreader.py
+++ tests/test_configreader.py
@@ -12,3 +12,15 @@ def test_match() -> None:
     assert reader.match(os.path.join("dir", "som_pattern"))[0]
     assert reader.match(os.path.join("dir", "some_pattern"))[0]
     assert not reader.match(os.path.join("dir", "sompattern"))[0]
+
+
+def test_match_with_incorrect_config_path() -> None:
+    config_path = os.path.join("tests", "false_path.txt")
+    reader = ConfigReader(config_path)
+
+    assert not reader.match(os.path.join("dir", "some_file.pyc"))[0]
+    assert not reader.match(os.path.join("dir", "other_file.pyo"))[0]
+    assert not reader.match(os.path.join("dir", "other_file.py"))[0]
+    assert not reader.match(os.path.join("dir", "som_pattern"))[0]
+    assert not reader.match(os.path.join("dir", "some_pattern"))[0]
+    assert not reader.match(os.path.join("dir", "sompattern"))[0]
